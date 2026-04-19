cf72962bb120b3e2e7530b07d899aaaf64065b86
JuniorAww <50590566+junioraww@users.noreply.github.com>
2026-04-19 11:50:36 +0200

    fix: cert reissuing & add config recommendations

--- sync.sh
+++ sync.sh
@@ -1,1 +1,1 @@
-rsync -avP --exclude='data/' --exclude='bot/node_modules' ./ techie@vip:/opt/hysteria
+rsync -avP --exclude='data/' --exclude='hysteria/acme' --exclude='bot/node_modules' ./ techie@vip:/opt/hysteria
